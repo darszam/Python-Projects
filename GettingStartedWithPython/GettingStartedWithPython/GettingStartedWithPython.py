abd70168724f64a376926ef9c74741b8b4bc1f1e
DandeOne <dande2@o2.pl>
2018-07-18 18:04:00 +0200

    Basic functions

--- GettingStartedWithPython/GettingStartedWithPython/GettingStartedWithPython.py
+++ GettingStartedWithPython/GettingStartedWithPython/GettingStartedWithPython.py
@@ -20,3 +20,20 @@ print (dictionary)
 # Set
 tst_set = set(['Jane','Mark','Stephen'])
 print(tst_set)
+
+# Functions
+def f(n):
+    return n*2
+print()
+print(f(2))
+
+def callfunction(f,n):
+    return f(n)
+
+print()
+print(callfunction(f,1))
+
+def callfunctionsquared(f,n):
+    return f(f(n))
+print()
+print(callfunctionsquared(f,2))
