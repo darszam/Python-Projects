f0a5b8162b47d5a311d2327e88053b69c8e18f64
DandeOne <dande2@o2.pl>
2018-08-01 00:48:55 +0200

    First model - Decision Tree Regressor and validating accuracy by mean absolute error

--- ML/houseprices.py
+++ ML/houseprices.py
@@ -15,3 +15,28 @@ print ()
 
 lot_area_and_year_built = data[['LotArea','YearBuilt']]
 print(lot_area_and_year_built.describe())
+
+# Creating learn model
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+y = data.SalePrice
+
+predictors = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr','TotRmsAbvGrd']
+
+X = data[predictors]
+
+my_model = DecisionTreeRegressor()
+my_model.fit(X,y)
+print("Making predictions for these houses")
+print(X.head())
+print("Predictions:")
+print(my_model.predict(X.head()))
+
+print(mean_absolute_error(y,my_model.predict(X)))
+
+train_X, val_X, train_y, val_y = train_test_split(X,y,random_state=0)
+my_model.fit(train_X, train_y)
+
+val_predictions = my_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
