dea085d9fba3737e6c5b0acabd54df0b93d9dd4f
DandeOne <dande2@o2.pl>
2018-08-29 00:05:25 +0200

    Activating models and checking api with django shell

--- Web/MyPollSite/MyPollSite/MyPollSite/settings.py
+++ Web/MyPollSite/MyPollSite/MyPollSite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Add your apps here to enable them
+    'polls.apps.pollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
