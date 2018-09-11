7a25459ba17d922e4aa217b911d671b023afe17a
DandeOne <dande2@o2.pl>
2018-09-12 00:20:41 +0200

    Created index site in templates

--- Web/MyPollSite/MyPollSite/polls/views.py
+++ Web/MyPollSite/MyPollSite/polls/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-publish_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 def testsite(request):
     return HttpResponse("Hello, world. This is another polls test site.")
 def detail(request, question_id):
