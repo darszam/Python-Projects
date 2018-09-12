72baa56aef3d0c67d2d258e7bc75ba04412714ab
DandeOne <dande2@o2.pl>
2018-09-13 00:04:08 +0200

    Using 404 error in get_object_or_404

--- Web/MyPollSite/MyPollSite/polls/views.py
+++ Web/MyPollSite/MyPollSite/polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
-
+from django.http import Http404
 def index(request):
     latest_question_list = Question.objects.order_by('-publish_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -10,7 +10,14 @@ def index(request):
 def testsite(request):
     return HttpResponse("Hello, world. This is another polls test site.")
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #try:
+        #question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+        #raise Http404("Question does not exist")
+    #return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
