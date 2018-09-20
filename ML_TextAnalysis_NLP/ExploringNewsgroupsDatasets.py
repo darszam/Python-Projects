09750056d5824fa8330aaec535842256042f58e2
DandeOne <dande2@o2.pl>
2018-09-20 13:46:21 +0200

    Importing newsgroups data and printing basic information and visualizations

--- ML_TextAnalysis_NLP/ExploringNewsgroupsDatasets.py
+++ ML_TextAnalysis_NLP/ExploringNewsgroupsDatasets.py
@@ -2,3 +2,17 @@ from nltk.corpus import names
 print (names.words()[:10])
 
 print(len(names.words()))
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+groups = fetch_20newsgroups()
+print(groups.keys())
+
+print(groups['target_names'])
+
+print(groups.target)
+
+import seaborn as sns
+sns.distplot(groups.target)
+
+import matplotlib.pyplot as plt
+plt.show()
