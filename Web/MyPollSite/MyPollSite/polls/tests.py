ab1c46588f576dfeb45a7a792f9f95894ed7078d
DandeOne <dande2@o2.pl>
2018-09-25 01:05:22 +0200

    Simple test

--- Web/MyPollSite/MyPollSite/polls/tests.py
+++ Web/MyPollSite/MyPollSite/polls/tests.py
@@ -6,7 +6,12 @@ Replace this with more appropriate tests for your application.
 """
 
 import django
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -23,3 +28,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose publish_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(publish_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
